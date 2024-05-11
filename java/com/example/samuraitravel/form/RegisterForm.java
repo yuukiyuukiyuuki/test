8103849208049ab9999f2f4a3e5502d651830233
yuukiyuukiyuuki <128203554+yuukiyuukiyuuki@users.noreply.github.com>
2024-05-11 13:47:44 +0900

    追加

---
+++ java/com/example/samuraitravel/form/RegisterForm.java
@@ -0,0 +1,20 @@
+package com.example.samuraitravel.form;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterForm {
+	
+	private Integer houseId;
+	
+	private Integer userId;
+	
+	private String star;
+	
+	private String review;
+
+}
