53973aebd32cb08d49a417d64ec18032e7e6b6d8
yuukiyuukiyuuki <128203554+yuukiyuukiyuuki@users.noreply.github.com>
2024-05-11 13:46:49 +0900

    Delete resources directory

--- resources/static/js/stripe.js
+++
@@ -1,9 +0,0 @@
-const stripe = Stripe('pk_test_51PACGEEidPDeDeb4a3ociLz40wnHXz680owe6AsW215y2S1uKSnrkF5YjuMAfYgDpELMIAZXgr9q6Buxch1beJnA00xc6Hvqs7');
- const paymentButton = document.querySelector('#paymentButton');
- 
- paymentButton.addEventListener('click', () => {
-   stripe.redirectToCheckout({
-     sessionId: sessionId
-   })
- });
- 
